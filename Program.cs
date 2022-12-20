57e82a8992f28b75e8d2e364e0edad9e5423066d
Subtype3860 <booravoi@gmail.com>
2022-12-20 17:43:24 +0300

    Finish project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace Task13._6._1;
+
+internal class Program
+{
+    public static void Main()
+    {
+        const string text = "Простое слово";
+        var textList = ReadTextFile.TextRead();
+        var textLinkedList = new LinkedList<string>(textList!);
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        textList!.Add(text);
+        stopWatch.Stop();
+        var timeSpanList = stopWatch.Elapsed;
+        Console.WriteLine($"Время вставки в List<T> равно: {timeSpanList}");
+        stopWatch.Start();
+        textLinkedList.AddFirst(text);
+        stopWatch.Stop();
+        var timeSpanLinkedList = stopWatch.Elapsed;
+        Console.WriteLine($"Время вставки в LinkedList<T> равно: {timeSpanLinkedList}");
+        Console.ReadKey();
+    }
+}
--- ReadTextFile.cs
+++ ReadTextFile.cs
@@ -2,5 +2,24 @@
 
 public class ReadTextFile
 {
-    
+    /// <summary>
+    /// Чтене из файла
+    /// </summary>
+    /// <returns>Получение массива типа string</returns>
+    public static List<string>? TextRead()
+    {
+        string fileText;
+        try
+        {
+            fileText = File.ReadAllText("Text.txt");
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
+        var charDelimetr = new char[] { ' ', '\r', '\n' };
+        var textArrey = fileText.Split(charDelimetr, StringSplitOptions.RemoveEmptyEntries).ToList();
+        return textArrey;
+    }
 }
